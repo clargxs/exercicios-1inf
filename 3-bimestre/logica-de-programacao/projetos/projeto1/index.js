67b0e1eb585e22087dc3c05797ecff623b111cae
Ana Clara <anaclarags180@gmail.com>
2026-09-11 09:53:41 -0300

    Implement purchase summary calculation in index.js
    Added a complete purchase summary calculation including customer details, product information, and financial breakdown.

--- 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
+++ 3-bimestre/logica-de-programacao/projetos/projeto1/index.js
@@ -1,6 +1,31 @@
 // CRIE SUA SOLUÇÃO ABAIXO ================
-
-
+const cliente  = "Mariana";
+const sobrenome = "Silva";
+const NomeCompleto = cliente+" "+sobrenome;
+const cidade = "Fortaleza";
+const produto = "Notebook Pro";
+const categoria = "Notebook";
+const quantidade = 2;
+const descontoPercentual = 10;
+const preco = 3500;
+const valorPago = 7000;
+const subtotal = preco*quantidade;
+const desconto = (subtotal*descontoPercentual)/100;
+const valorFinal = subtotal-desconto;
+const troco = valorPago-valorFinal;
+const resumo = `Cliente:${NomeCompleto}
+Cidade:${cidade}
+Produto:${produto}
+Pategoria:${categoria}
+Quantidade:${quantidade}
+Preço:${preco}
+Subtotal:${subtotal}
+Desconto:${descontoPercentual}
+Valor do desconto:${desconto}
+Valor final:${valorFinal}
+Valor pago:${valorPago}
+Troco:${troco}`
+console.log(resumo)
 
 
 
